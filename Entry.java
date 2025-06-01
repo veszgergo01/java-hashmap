42d2b3ea47db8752d1725a5efedde8867da99b31
veszgergo <vesz.gergo@gmail.com>
2025-06-01 18:26:54 +0200

    Added EntryState (tombstone, empty, occupied) and usage in Entry

--- Entry.java
+++ Entry.java
@@ -1,9 +1,27 @@
 public class Entry<K, V> {
     public K key;
     public V value;
+    public EntryState state;
 
+    /**
+     * This constructor builds an Entry (key-value pair) naturally.
+     */
     public Entry(K key, V value) {
         this.key = key;
         this.value = value;
+        state = EntryState.OCCUPIUED;
+    }
+
+    /**
+     * This constructor can be used to create tombstones/empty entries.
+     * 
+     * @throws IllegalArgumentException if {@code entryState == EntryState.OCCUPIED}
+     */
+    public Entry(EntryState entryState) {
+        if (entryState.equals(EntryState.OCCUPIUED)) {
+            throw new IllegalArgumentException("Cannot create an falsely occupied entry.");
+        }
+
+        this.state = entryState;
     }
 }
---
+++ EntryState.java
@@ -0,0 +1,5 @@
+public enum EntryState {
+    TOMBSTONE,
+    EMPTY,
+    OCCUPIUED;
+}
